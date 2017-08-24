8611cfb031206605f1debe6fba8651e896c24348
Gerald Versluis <gerald@verslu.is>
2017-08-24 14:32:15 +0200

    Initial commit

---
+++ LeftToolbarSample/LeftToolbarSamplePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace LeftToolbarSample
+{
+    public partial class LeftToolbarSamplePage : ContentPage
+    {
+        public LeftToolbarSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleRight_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new NextPage());
+        }
+
+		private void HandleWrong_Clicked(object sender, System.EventArgs e)
+		{
+            Navigation.PushAsync(new NavigationPage(new NextPage()));
+		}
+    }
+}
---
+++ LeftToolbarSample/NextPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LeftToolbarSample
+{
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
